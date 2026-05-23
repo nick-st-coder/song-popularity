00daf0bd681b7f463fddbe3db5897cf6c01b3a6f
Nick <babukhnikita@gmail.com>
2026-05-23 15:49:41 +0200

    modified: first analysis of most popular genre added

--- notebooks/01_eda.py
+++ notebooks/01_eda.py
@@ -19,14 +19,60 @@ def _(pd):
     return (high_pop,)
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ---
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    By executing command below, we see, that the most popular genres are rock, pop and hip-hop with significant difference from other genres below them.
+    """)
+    return
+
+
 @app.cell
 def _(high_pop):
-    high_pop
+    # this line may be underlined as an error, but it's just static-analysis noise (warning).
+    high_pop.groupby(by='playlist_genre').size().sort_values(ascending=False).head(5)  # ty:ignore[no-matching-overload]
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ---
+    """)
     return
 
 
 @app.cell
 def _():
+    target_genres = ['rock', 'pop', 'hip-hop']
+    return (target_genres,)
+
+
+@app.cell
+def _(high_pop, target_genres):
+    result = high_pop[high_pop['playlist_genre'].isin(target_genres)].groupby(by='playlist_genre').agg({
+        'energy':'mean',
+        'danceability':'mean',
+        'playlist_genre':'count',
+        'loudness':'mean',
+        'liveness':'mean',
+        'valence':'mean',
+        'speechiness':'mean'
+    })
+    return (result,)
+
+
+@app.cell
+def _(result):
+    result
     return
 
 
