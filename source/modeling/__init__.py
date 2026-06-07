31556e8db3f2256e4d3a92c8a4494d261265e409
Nick <babukhnikita@gmail.com>
2026-06-07 17:40:25 +0200

    modified: separated function for getting params added, removed params from function in optuna

--- source/modeling/__init__.py
+++ source/modeling/__init__.py
@@ -1,1 +1,2 @@
 from .optuna import objective_lgbm
+from .params import get_lgbm_params
--- source/modeling/optuna.py
+++ source/modeling/optuna.py
@@ -1,11 +1,14 @@
 from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import cross_val_score, KFold
+from sklearn.model_selection import cross_val_score, KFold 
+from sklearn.compose import ColumnTransformer
+
 from lightgbm import LGBMRegressor
 import mlflow
 import pandas as pd
-from sklearn.compose import ColumnTransformer
 from optuna import Trial
+
+from .params import get_lgbm_params, get_rf_params
 
 # Random Forest
 
@@ -14,19 +17,7 @@ def objective_rf(trial:Trial,
                 X_train: pd.Dataframe, 
                 y_train: pd.DataFrame):
     
-    params = {
-        'n_estimators':trial.suggest_int('n_estimators', 200, 800),
-        'max_depth':trial.suggest_int('max_depth', 4, 8),
-        'min_samples_leaf':trial.suggest_int("min_samples_leaf", 20, 80),
-        'min_samples_split':trial.suggest_int("min_samples_split", 30, 120),
-        'max_features':trial.suggest_categorical(
-            "max_features", ['sqrt', 'log2', None]
-        ),
-
-        'random_state':4,
-        'verbose':1,
-        'n_jobs':-1
-    }
+    params = get_rf_params(trial)
 
     cv = KFold(
         n_splits=5,
@@ -67,21 +58,8 @@ def objective_lgbm(trial:Trial,
                    preprocess:ColumnTransformer, 
                    X_train: pd.Dataframe, 
                    y_train: pd.DataFrame):
-    params = {
-        'colsample_bytree':trial.suggest_float('colsample_bytree', 0.5, 1.0),
-        'subsample':trial.suggest_float('subsample', 0.5, 1.0),
-        'learning_rate':trial.suggest_float('learning_rate', 0.005, 0.3, log=True),
-        'max_depth':trial.suggest_int('max_depth', 5, 12),
-        'n_estimators':trial.suggest_int('n_estimators', 100, 1200),
-        'num_leaves':trial.suggest_int('num_leaves', 6, 255),
-        'reg_alpha':trial.suggest_float('reg_alpha', 1e-8, 10.0, log=True),
-        'reg_lambda':trial.suggest_float('reg_lambda', 1e-8, 10.0, log=True),
-        'min_child_samples':trial.suggest_int('min_child_samples', 5, 100),
 
-        'objective':'regression',
-        'random_state':42,
-        'verbosity':-1
-    }   
+    params = get_lgbm_params(trial)
 
     cv = KFold(
         n_splits=5,
